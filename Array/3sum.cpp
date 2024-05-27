d770ee4e685fe51aa1d03845a81ca11b6bb4e56c
Pallavit Saxena <121334016+soundmind07@users.noreply.github.com>
2024-05-27 18:13:32 +0530

    Create 3sum.cpp

---
+++ Array/3sum.cpp
@@ -0,0 +1,39 @@
+problem link:-
+  https://leetcode.com/problems/3sum/description/
+Code:-
+  class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(i>0 and nums[i]==nums[i-1])
+            continue;
+            int left=i+1,right=n-1;
+            while(left<right)
+            {
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum<0)
+                {
+                    left++;
+                }
+                else if(sum>0)
+                right--;
+                else
+                {
+                    ans.push_back({nums[i],nums[left],nums[right]});
+                    left++;
+                    right--;
+                    while(left<right and nums[left]==nums[left-1])
+                    left++;
+                    while(left<right and nums[right]==nums[right+1])
+                    right--;
+
+                }
+            }
+        }
+        return ans;
+    }
+};

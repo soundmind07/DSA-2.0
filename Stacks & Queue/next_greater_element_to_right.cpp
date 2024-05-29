0fbeacc32f588e03c593fec100743b24365f9630
Pallavit Saxena <121334016+soundmind07@users.noreply.github.com>
2024-05-29 14:51:06 +0530

    Create next_greater_element_to_right.cpp

---
+++ Stacks & Queue/next_greater_element_to_right.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printNGE(int arr[], int n)
+{
+	stack<int> s;
+
+	
+	s.push(arr[0]);
+
+	// iterate for rest of the elements
+	for (int i = 1; i < n; i++) {
+
+		if (s.empty()) {
+			s.push(arr[i]);
+			continue;
+		}
+
+	
+		while (s.empty() == false && s.top() < arr[i]) {
+			cout << s.top() << " --> " << arr[i] << endl;
+			s.pop();
+		}
+
+	
+		s.push(arr[i]);
+	}
+
+	while (s.empty() == false) {
+		cout << s.top() << " --> " << -1 << endl;
+		s.pop();
+	}
+}
